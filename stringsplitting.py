12770e84f7b9c3d3a8ea3cec6e5a68e51f754811
yesvanthra <132344842+yesvanthra@users.noreply.github.com>
2023-08-11 17:30:48 +0530

    Delete stringsplitting.py

--- stringsplitting.py
+++
@@ -1,5 +0,0 @@
-import re
-def stringsplittingwithdelimiter(text, delimiters):
-    result=re.findall('[^%s]+' % re.escape(delimiters), text)
-    print(result)
-stringsplittingwithdelimiter("sdfkdjsadfsd diweiw;1231:foo", ' ;:')
